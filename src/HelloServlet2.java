2e7b508e8eb6e0bbcffd8520de155a10a8facfce
yh1231230 <1106885620@qq.com>
2021-04-19 21:43:00 +0800

    JavascriptTest

---
+++ src/HelloServlet2.java
@@ -0,0 +1,17 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class HelloServlet2 extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.getWriter().println("<h1>Hello Servlet </h1>");
+            response.getWriter().println(new Date().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
